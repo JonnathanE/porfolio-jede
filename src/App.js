d118556b6e365ac9e46a91f7ea5cd12890e249fc
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-02-09 18:03:30 -0500

    config tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
 function App() {
   return (
-    <div className="App bg-slate-800">
+    <div className="App bg-bookmark-purple">
       Hola JEDE
+      <div className="App  bg-bookmark-red">
+        Hola JEDE
+      </div>
+      <div className="App  bg-bookmark-blue">
+        Hola JEDE
+      </div>
+      <div className="App  bg-bookmark-grey">
+        Hola JEDE
+      </div>
+      <div className="App  bg-bookmark-white">
+        Hola JEDE
+      </div>
+      <div className="App  bg-bookmark-orange">
+        Hola JEDE
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+        "bookmark-orange": "#FD7014"
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screen: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    },
   },
   plugins: [],
 }
