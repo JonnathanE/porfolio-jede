13f1b471625c4376e0f1955684c2a9d1e694ab69
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-08-17 02:37:49 -0500

    Add info

--- src/containers/certifications/Certifications.jsx
+++ src/containers/certifications/Certifications.jsx
@@ -1,9 +1,10 @@
-import TitleSection from '../../atom/titleSection/TitleSection';
-import { SiUdemy } from 'react-icons/si';
-import tw, { styled } from 'twin.macro';
-import Edteam from '../../img/edeteam-icon.png';
+import TitleSection from "../../atom/titleSection/TitleSection";
+import { SiUdemy } from "react-icons/si";
+import tw, { styled } from "twin.macro";
+import Edteam from "../../img/edeteam-icon.png";
+import CodigoFacilito from "../../img/codigofacilito-icon.jpeg";
 
-import { certifications } from '../../data';
+import { certifications } from "../../data";
 
 const Contianer = tw.div`
 relative top-20 pb-20 mb-16 p-2 flex flex-col items-center bg-bookmark-white dark:bg-gray-800
@@ -18,8 +19,8 @@ bg-white dark:bg-gray-600 rounded-lg shadow-md p-6
 `;
 
 const ListItem = styled.li`
-${tw`mb-5`}
-${({ hasBorder }) => hasBorder && tw`border-b-2`}
+  ${tw`mb-5`}
+  ${({ hasBorder }) => hasBorder && tw`border-b-2`}
 `;
 
 const Title = tw.span`
@@ -40,46 +41,61 @@ const Icon = tw.img`
 `;
 
 const Certifications = () => {
-    return (
-        <div id='certification'>
-            <Contianer>
-                <TitleSection>Certifications</TitleSection>
-                <Wrapp>
-                    <List>
-                        {certifications.map((c, index) => (
-                            <ListItem key={c.id} hasBorder={index === (certifications.length - 1) ? false : true}>
-                                <div className='flex'>
-                                    <div>
-                                        {c.business === 'Udemy' && <SiUdemy size={48} className='dark:text-white'/>}
-                                        {c.business === 'EDteam' && <Icon src={Edteam}/>}
-                                    </div>
-                                    <div className='ml-3'>
-                                        <div>
-                                            <a href={c.url} target={'blank'} className='flex flex-col'>
-                                                <div>
-                                                    <Title>{c.name}</Title>
-                                                </div>
-                                                <SubTitle>{c.business}</SubTitle>
-                                                {c.expedition
-                                                    ? (<SubTitle>
-                                                        Expedition: {c.expedition} - {c.expiration ? c.expiration : 'No expiration date'}
-                                                    </SubTitle>)
-                                                    : null}
-                                                <SubTitle>Credential ID {c.idCertification}</SubTitle>
-                                            </a>
-                                        </div>
-                                        <div className='mb-8 mt-4'>
-                                            <BtnLink href={c.url} target='blank'>See credential</BtnLink>
-                                        </div>
-                                    </div>
-                                </div>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Wrapp>
-            </Contianer>
-        </div>
-    )
-}
+  return (
+    <div id="certification">
+      <Contianer>
+        <TitleSection>Certifications</TitleSection>
+        <Wrapp>
+          <List>
+            {certifications.map((c, index) => (
+              <ListItem
+                key={c.id}
+                hasBorder={index === certifications.length - 1 ? false : true}
+              >
+                <div className="flex">
+                  <div>
+                    {c.business === "Udemy" && (
+                      <SiUdemy size={48} className="dark:text-white" />
+                    )}
+                    {c.business === "EDteam" && <Icon src={Edteam} />}
+                    {c.business === "CódigoFacilito" && (
+                      <Icon src={CodigoFacilito} />
+                    )}
+                  </div>
+                  <div className="ml-3">
+                    <div>
+                      <a
+                        href={c.url}
+                        target={"blank"}
+                        className="flex flex-col"
+                      >
+                        <div>
+                          <Title>{c.name}</Title>
+                        </div>
+                        <SubTitle>{c.business}</SubTitle>
+                        {c.expedition ? (
+                          <SubTitle>
+                            Expedition: {c.expedition} -{" "}
+                            {c.expiration ? c.expiration : "No expiration date"}
+                          </SubTitle>
+                        ) : null}
+                        <SubTitle>Credential ID {c.idCertification}</SubTitle>
+                      </a>
+                    </div>
+                    <div className="mb-8 mt-4">
+                      <BtnLink href={c.url} target="blank">
+                        See credential
+                      </BtnLink>
+                    </div>
+                  </div>
+                </div>
+              </ListItem>
+            ))}
+          </List>
+        </Wrapp>
+      </Contianer>
+    </div>
+  );
+};
 
-export default Certifications
+export default Certifications;
